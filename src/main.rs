cf3db7c7c26184c852159e58ee2fca07ea4ac90b
dev1codcc <dev1@codeapi.cc>
2023-11-06 21:37:12 +0800

    new rust project

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use ferris_says::say; //using the crate
+use std::io::{stdout, BufWriter};
+
+fn main(){
+    let stdout = stdout();
+    let message = String::from("Hello fellow Rustaceans!");
+    let width = message.chars().count();
+
+    let mut writer = BufWriter::new(stdout.lock());
+    say(&message, width, &mut writer).unwrap();
+}
+/*
+
+fn main() {
+    println!("Hello, world!");
+}
+
+*/
