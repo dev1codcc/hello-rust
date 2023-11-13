59708522d39e0d3a2c49ea53fb3c27eb6b1eb628
Bryan Lu <chun.lu@siemens.com>
2023-11-13 11:23:44 +0800

    recheck the code, ok

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,16 @@
 //use ferris_says::say; //using the crate
 use std::fs;
 use serde_json;
-use tiberius::{Client, Config, AuthMethod};
-use async_std::net::TcpStream;
+
 
 fn main(){
     let data = fs::read_to_string("./sample.json")
         .unwrap();
 
-    let _json: serde_json::Value = serde_json::from_str(&data)
+    let json: serde_json::Value = serde_json::from_str(&data)
         .expect("JSON does not have correct format");
 
-    //dbg!(json);
-
-    dbg!(json.as_object());
+    dbg!(json);
 
     //Analyz json
 }
